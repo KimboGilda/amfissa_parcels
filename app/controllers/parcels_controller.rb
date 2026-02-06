c913dcdfa458ad166b6da957e6a9ec17a98393bd
Nikos Bakogiannis <nbakojohn13@gmail.com>
2026-02-06 21:37:48 +0100

    add new actions

--- app/controllers/parcels_controller.rb
+++ app/controllers/parcels_controller.rb
@@ -7,4 +7,31 @@ class ParcelsController < ApplicationController
     @parcel = Parcel.find(params[:id])  
   end
 
+  def new
+    @parcel = Parcel.new
+  end
+
+  def create
+    @parcel = Parcel.new(parcel_params)
+
+    if @parcel.save
+      redirect_to parcel_path(@parcel), notice: 'Parcel was successfully created.'
+      else
+        render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @parcel = Parcel.find(params[:id])
+    @parcel.destroy
+
+    redirect_to parcel_path, notice: 'Parcel was successfully deleted'
+  end
+
+  private
+
+  def parcel_params
+    params.require(:parcel).permit(:name, :kaek, :area)
+  end
+
 end
--- app/models/parcel.rb
+++ app/models/parcel.rb
@@ -1,2 +1,5 @@
 class Parcel < ApplicationRecord
+  validates :name, presence: true
+  validates :kaek, presence: true
+  validates :area, presence: true
 end
