268d6ad4c92a5b785cc5f7e1d8d78b84c15497a3
Nikos Bakogiannis <nbakojohn13@gmail.com>
2026-02-06 22:03:24 +0100

    :art: refactoring

--- app/javascript/controllers/toggle_button_controller.js
+++ app/javascript/controllers/toggle_button_controller.js
@@ -9,14 +9,14 @@ export default class extends Controller {
   toggle() {
     console.log(
       "Initial opacity:",
-      window.getComputedStyle(this.coordinatesTarget).opacity
+      window.getComputedStyle(this.coordinatesTarget).opacity,
     );
     if (window.getComputedStyle(this.coordinatesTarget).opacity === "0") {
       this.coordinatesTarget.style.opacity = "1";
       this.coordinatesTarget.style.pointerEvents = "auto";
       this.coordinatesTarget.scrollIntoView({
         behavior: "smooth", // Smooth scrolling
-        block: "start" // Align to the top of the viewport
+        block: "start", // Align to the top of the viewport
       });
     } else {
       this.coordinatesTarget.style.opacity = "0";
